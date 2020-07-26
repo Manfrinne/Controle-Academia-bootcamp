4b84e1fadf4b1e4047a06e24508c54d22b7f2250
Manfrinne <m4nfrinne@gmail.com>
2020-07-26 01:32:05 -0300

    Set 'created_at' variable to 'Intl'

--- instructors.js
+++ instructors.js
@@ -18,7 +18,7 @@ exports.show = function(req, res) {
         ...foundInstructor, // spread operator
         age: age(foundInstructor.birth),
         services: foundInstructor.services.split(","),
-        created_at: "",
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
     }
 
     return res.render("instructors/show", {instructor})
