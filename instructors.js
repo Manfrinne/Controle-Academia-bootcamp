403e234f266e0c059fb88e8b32a8558d58bfa556
Manfrinne <m4nfrinne@gmail.com>
2020-07-26 23:55:40 -0300

    setting universal dates and correcting input date on 'edit' page

--- instructors.js
+++ instructors.js
@@ -1,12 +1,61 @@
 const fs = require('fs')
 const data = require('./data.json')
-const {age} = require('./utils') // {age} as chaves é para desestruturação do objeto
+const {age, date} = require('./utils')
+// {age} com as chaves é para
+// desestruturação do objeto
+
+// create input "create page"
+exports.post = function(req, res) {
+
+  // Validação dos dados
+  const keys = Object.keys(req.body)
+  for (key of keys) {
+      // req.body.key == ""
+      // key ==> todos dados introduzidos no input
+      if (req.body[key] == "") { // gerando array para separar objetos
+          return res.send('Please, fill in all fields.')
+      }
+  }
+
+  // Desestruturação de objetos
+  let { avatar_url, birth, name, services, gender } = req.body
+
+  // timestamp
+  birth = Date.parse(req.body.birth)
+  const created_at = Date.now()
+
+  // criar uma variável para identificar um objeto isoladamente
+  const id = Number(data.instructors.length + 1)
+
+  // o método ".push" não deixa um novo objeto subscrever o objeto existente
+  data.instructors.push({
+      id,
+      avatar_url,
+      name,
+      birth,
+      gender,
+      services,
+      created_at,
+  })
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+
+      // Esse condicional é só para nossa aplicação não cair caso, por algum
+      // motivo, o arquivo 'data.json' não puder ser criado
+      if (err) {return res.send("Write file error!")}
+
+      return res.redirect("/instructors")
+  })
+
+  // return res.send(req.body)
+}
 
 // function show data.json
 exports.show = function(req, res) {
 
-    // routes.get('/instructors/:id', instructors.show)
-    const {id} = req.params
+    // com toda essa estrutura eu consigo isolar os dados de
+    // um instrutor específico.
+    const {id} = req.params // routes.get('/instructors/:id', instructors.show)
 
     const foundInstructor = data.instructors.find(function(instructor) {
         return instructor.id == id
@@ -18,50 +67,28 @@ exports.show = function(req, res) {
         ...foundInstructor, // spread operator
         age: age(foundInstructor.birth),
         services: foundInstructor.services.split(","),
-        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
+        created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at),
     }
 
     return res.render("instructors/show", {instructor})
 }
 
-// create input "create page"
-exports.post = function(req, res) {
+// função para editar dados do instrutor
+exports.edit = function(req, res) {
+  const {id} = req.params // routes.get('/instructors/:id', instructors.show)
 
-    // Validação dos dados
-    const keys = Object.keys(req.body)
-    for (key of keys) {
-        // req.body.key == ""
-        // key ==> todos dados introduzidos no input
-        if (req.body[key] == "") { // gerando array para separar objetos
-            return res.send('Please, fill in all fields.')
-        }
-    }
+  // variável para isolar um objeto específico do meu "data.json"
+  const foundInstructor = data.instructors.find(function(instructor) {
+      return instructor.id == id
+  })
 
-    // Desestruturação de objetos
-    let { avatar_url, birth, name, services, gender } = req.body
+  if (!foundInstructor) return res.send("Instructor not found!")
 
-    // timestamp
-    birth = Date.parse(req.body.birth)
-    const created_at = Date.now()
-    const id = Number(data.instructors.length + 1)
 
-    data.instructors.push({
-        id,
-        avatar_url,
-        name,
-        birth,
-        gender,
-        services,
-        created_at,
-    })
+  const instructor = {
+    ...foundInstructor,
+    birth: date(foundInstructor.birth)
+}
 
-    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-
-        if (err) {return res.send("Write file error!")}
-        // Esse condicional é só para a aplicação não cair caso, por algum motivo, o file 'data.json' não puder ser criado
-
-        return res.redirect("/instructors")
-    })
-
-    // return res.send(req.body)
+  return res.render("instructors/edit", {instructor})
 }
--- utils.js
+++ utils.js
@@ -12,5 +12,23 @@ age: function age(timestamp) {
   }
 
   return age
+},
+
+// função para resolver o erro
+// na página de editar dados
+date: function(timestamp) {
+  const date = new Date(timestamp)
+
+  const year = date.getUTCFullYear()
+
+  // '+ 1' porque o 'getUTCMonth()' retorna
+  // um valor de 0 a 11
+  const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+
+  const day = `0${date.getUTCDate()}`.slice(-2)
+
+  // return yyyy-mm-dd
+  return (`${year}-${month}-${day}`)
 }
+
 }
